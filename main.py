c6e587e321623ffb3150f7bca561ffda504b3e7f
Krut-Oskar <53490024+Krut-Oskar@users.noreply.github.com>
2020-10-01 17:30:28 +0200

    gjort ganska stora ändringar
    Gått från catfacts till Donald trump och Chuck Norris quotes istälelt.

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
-print("Hello World")
+from api_handler import ApiHandler
+
+
+while True: 
+    print("Choose a quote from either Chuck Norris or Donald Trump \nChuck Norris = 1 \nDonald Trump = 2")
+    choice = input("->")
+    if choice == "1":        
+        random_chuck_quote = ApiHandler("https://api.chucknorris.io/jokes/random")
+        print(random_chuck_quote.getQuote())
+    elif choice == "2":
+        donald_quote = ApiHandler("https://www.tronalddump.io/random/quote")
+        print(donald_quote.getQuote())
+    else:
+        print("please choose 1 or 2")
+
+
+
+
+
+
+
+
+
